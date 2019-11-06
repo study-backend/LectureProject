afc33446e397fd23f21f78fadb9db510fd57a43e
kosta <kosta@192.168.0.105>
2019-11-06 17:52:47 +0900

    [강한규]lecture biz 수정

--- src/edu/kosta/lecture/service/LectureService.java
+++ src/edu/kosta/lecture/service/LectureService.java
@@ -2,25 +2,23 @@ package edu.kosta.lecture.service;
 
 import java.util.List;
 
-import edu.kosta.lecture.biz.LectureBiz;
 import edu.kosta.lecture.dao.LectureDAO;
 import edu.kosta.lecture.model.Lecture;
 
+import edu.kosta.lecture.biz.LectureBiz;
+
 public class LectureService implements LectureBiz {
 
-	private LectureDAO dao = new LectureDAO(); //DAOÀÇ  
-	
-	public List<Lecture> selectAll() {
-		List<Lecture> list = dao.selectAll();
-		
-		return list;
-		
-	}
-	
 	@Override
 	public void SelectStudentMap(int lectureId) {
 		// TODO Auto-generated method stub
 		
 	}
-	
+
+	@Override
+	public void SelectTeacherMap(int lectureId) {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
